e908f5109190550abb4acbfc820534c2f4a294d5
mwangiowennjugunasmith <owen.njugun@student.moringa.com>
2023-09-24 23:07:29 +0300

    completed asignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+function writeCards(names, eventName) {
+    const thankYouMessages = [];
+  
+    for (let i = 0; i < names.length; i++) {
+      const message = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+      thankYouMessages.push(message);
+    }
+  
+    return thankYouMessages;
+  }
+  
+  
+  function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+      console.log(i);
+    }
+  }
+  
+  
+  module.exports = {
+    writeCards,
+    countDown,
+  };
